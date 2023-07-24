d278823343b68185e08f3e1aaf7823ffffcf03ba
TechMishima <t.mishima242@gmail.com>
2023-07-24 12:04:00 +0900

    1-1正しく編集できた場合は、詳細ページへ遷移すること

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -12,7 +12,7 @@ class PrototypesController < ApplicationController
   def create
     @prototype = Prototype.new(prototype_params)
     if @prototype.save
-      redirect_to prototype_path(@prototype.id)
+      redirect_to root_path
     else
       render :new, status: :unprocessable_entity
     end
@@ -31,7 +31,7 @@ class PrototypesController < ApplicationController
   def update
     @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
-      redirect_to root_path
+      redirect_to prototype_path(@prototype.id)
     else
       render :edit, status: :unprocessable_entity
     end
