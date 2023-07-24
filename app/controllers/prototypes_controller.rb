9ff7e08fa70d821c17bac2f49537ce2995e319c0
TechMishima <t.mishima242@gmail.com>
2023-07-24 12:12:00 +0900

    他のユーザーのプロトタイプ編集ページに遷移できなくする

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :edit]
 
   def index
     @prototype = Prototype.includes(:user)
